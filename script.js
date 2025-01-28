26f031dabd3b0177c7e3d7933c3bdfcf36576f9b
rosenode <226mmendes@frhsd.com>
2025-01-28 14:58:02 -0500

    1/28/25 PROGRESS

---
+++ script.js
@@ -0,0 +1,67 @@
+function changeImageAtomic() {
+    var img = document.getElementById('eventImage');
+    img.src = 'atomicbomb.jpg';
+}
+function changeTitleAtomic() {
+    document.getElementById('eventTitle').innerHTML="THE FIRST ATOMIC BOMB";
+}
+function changeInfoAtomic() {
+    document.getElementById('infoText').innerHTML="On October 3, 1952, the United Kingdom detonated its first atomic bomb. Research was first initialized in 1946, where a secret cabinet committee invested time into developing an atomic bomb to remain a world power. In May of 1952, it was then announced that the test would be conducted on the inhabited islands of Monte Bello. The bomb vaporized everything within the islands’ radius, starting fires and appearing to be about 6,000 feet as the cloud rose.";
+    document.getElementById('infoLabel').innerHTML="The resulting blast of the 1952 bomb test.";
+}
+
+/////////////////////////////////////////////
+
+function changeImageProc() {
+    var img = document.getElementById('eventImage');
+    img.src = '';
+}
+function changeTitleProc() {
+    document.getElementById('eventTitle').innerHTML="THE THANKSGIVING PROCLAMATION";
+}
+function changeInfoProc() {
+    document.getElementById('infoText').innerHTML="During his administration, Abraham Lincoln authorized the permanence of the national day of Thanksgiving on October 3rd, 1863. This was first brought to his attention by a magazine editor named Sarah Josepha Hale, who addressed the desire of the American people to have an official day reserved for Thanksgiving brought to national recognition.";
+    document.getElementById('infoLabel').innerHTML="";
+}
+
+/////////////////////////////////////////////
+
+function changeImagePoe() {
+    var img = document.getElementById('eventImage');
+    img.src = '';
+}
+function changeTitlePoe() {
+    document.getElementById('eventTitle').innerHTML="THE DAY OF EDGAR ALLAN POE'S DEATH";
+}
+function changeInfoPoe() {
+    document.getElementById('infoText').innerHTML="Edgar Allan Poe, a famous short-story writer renowned for his poems “The Raven” and “To Helen,”  was found dead under the influence of an intoxicant beneath a gutter. His mysterious death caused many to speculate the cause; meeting unsavory people, rabies, a brain tumor, carbon monoxide poisoning, or maybe even mercury poisoning. Overall, it’s a death that has left many doctors and fans pondering.";
+    document.getElementById('infoLabel').innerHTML="";
+}
+
+/////////////////////////////////////////////
+
+function changeImageBattle() {
+    var img = document.getElementById('eventImage');
+    img.src = '';
+}
+function changeTitleBattle() {
+    document.getElementById('eventTitle').innerHTML="THE BATTLE OF PHILIPPI";
+}
+function changeInfoBattle() {
+    document.getElementById('infoText').innerHTML="The Battle of Philippi was the climactic battle that followed the assassination of Julius Caesar, some time in 44 BCE. The Battle of Philippi brought ensuing chaos and confusion, proving to be a brutal killing match and the destruction of its old Republican constitution. Caesar loyalists seized control of Rome and its western provinces, setting out to quell its rebellions. The Triumvirates came out victorious against the optimates, having launched a successful frontal assault on October 3rd.";
+    document.getElementById('infoLabel').innerHTML="";
+}
+
+/////////////////////////////////////////////
+
+function changeImageIraq() {
+    var img = document.getElementById('eventImage');
+    img.src = '';
+}
+function changeTitleIraq() {
+    document.getElementById('eventTitle').innerHTML="IRAQ WINS INDEPENDENCE";
+}
+function changeInfoIraq() {
+    document.getElementById('infoText').innerHTML="The British had first seized Iraq from Ottoman Turkey during WWI, granted a mandate by the League of Nations to govern it in 1920. Twelve years later, under the League of Nations, Iraq would be granted a termination of Britain’s mandate for its admission- officially making Iraq an independent nation.";
+    document.getElementById('infoLabel').innerHTML="";
+}
